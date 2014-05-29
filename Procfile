8a7ead683e695348a24b85f9efb70d3a48317ad1
lpld <lsivashov@gmail.com>
2014-05-30 00:50:23 +0300

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: java -cp target/classes com.github.lpld.heroku.Worker
---
+++ src/main/java/com/github/lpld/heroku/Worker.java
@@ -0,0 +1,11 @@
+package com.github.lpld.heroku;
+
+/**
+ * @author leopold
+ * @since 5/30/14
+ */
+public class Worker {
+    public static void main(String[] args) {
+        System.out.println("Working!");
+    }
+}
